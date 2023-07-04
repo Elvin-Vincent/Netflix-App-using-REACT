6ca0d1d3c7c244be4d832dc6817d9677b40fa561
Elvin <elvinva2271@gmail.com>
2023-07-04 09:08:15 +0530

    added more categoy

--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import './RowPost.css'
 function RowPost() {
-  const [state, setstate] = useState(null)
-  const [pop1,setpop1]=useState(null)
+  const [popular, setpopular] = useState(null)
+  const [trending,settreding]=useState(null)
+  const [upcoming,setupcoming]=useState(null)
+  const [nowplaying,setnowplaying]=useState(null)
   useEffect(()=>{
     const options = {
       method: 'GET',
@@ -11,32 +13,44 @@ function RowPost() {
         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYjcxYjhjYmQ0ZjNjMGZmNzQ0NzQxZDQxYjA5Yzk1NyIsInN1YiI6IjY0YTE1ZjZmOGMwYTQ4MDEwMTc3MTFjMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KZQ4Di2xJs-cQ4L9ygCJ7z7tiXougY-i3VKE3l6Lt3Q'
       }
     };
-    
-    fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
-    
+    //now playing
+    fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
       .then(response => response.json())
-      
-      .then(readabledata => {setstate(readabledata)
+      .then(readabledata =>{setnowplaying(readabledata)
+        console.log(readabledata)
+      })
+      .catch(err => console.error(err));
+    //popular movies
+    fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+      .then(response => response.json())
+      .then(readabledata => {setpopular(readabledata)
       console.log(readabledata)
       })
-  
-  .catch(err => console.error(err));
-      fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US', options)
-  .then(response => response.json())
-  .then(data => {setpop1(data)
-    console.log(data)
+      .catch(err => console.error(err));
+  //trending movies
+    fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US', options)
+      .then(response => response.json())
+      .then(readabledata => {settreding(readabledata)
+      console.log(readabledata)
+    })
+    .catch(err => console.error(err));
+  //upcoming movies
+  fetch('https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1', options)
+    .then(response => response.json())
+    .then(readabledata => {setupcoming(readabledata)
+    console.log(readabledata)
   })
   .catch(err => console.error(err));
 
   },[])
   return (
     <div className='row'>
-        <h2>Play Now</h2>
+        <h1>Now Playing</h1>
         <div className="posters">
           
           {
             
-            state?.results.map((obj)=>{
+            nowplaying?.results.map((obj)=>{
               return(
                 <div className='poster' key={obj.title}>
                   
@@ -47,11 +61,42 @@ function RowPost() {
             })
           }
           </div>
-          <h1>Popular Movies</h1>
+        <h1>Popular Movies</h1>
+        <div className="posters">
+          
+          {
+            
+            popular?.results.map((obj)=>{
+              return(
+                <div className='poster' key={obj.title}>
+                  
+                  
+                  <img width={170} src={`https://image.tmdb.org/t/p/w500${obj.poster_path}`} alt='poster'/>
+                  </div>
+              )
+            })
+          }
+          </div>
+          <h1>Top Rated Movies</h1>
           <div className='posters'>
             
           {
-            pop1?.results.map((obj)=>{
+            trending?.results.map((obj)=>{
+              return(
+                <div className='poster' key={obj.title}>
+                  
+                  
+                  <img width={170} src={`https://image.tmdb.org/t/p/w500${obj.poster_path}`} alt='poster'/>
+                  </div>
+              )
+            })
+          }
+          </div>
+          <h1>Upcoming Movies</h1>
+          <div className='posters'>
+            
+          {
+            upcoming?.results.map((obj)=>{
               return(
                 <div className='poster' key={obj.title}>
                   
