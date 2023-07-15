6177f25689a7b590ae7ecd28d2af4bbefbc71347
Elvin <elvinva2271@gmail.com>
2023-07-15 16:38:08 +0530

    added main poster

--- src/Components/NavBar/Banner/Banner.js
+++ src/Components/NavBar/Banner/Banner.js
@@ -1,19 +1,58 @@
-import React from 'react'
-import './Banner.css'
+import React, { useEffect, useState } from 'react';
+import './Banner.css';
+
 function Banner() {
-  return (
-    <div className='banner'>
-        <div className='content'>
-            <h1 className='title'>MONEY HEIST</h1>
-            <div className='banner_button'>
-                <button className='button'>play</button>
-                <button className='button'>Mylist</button>
+  const [nowplaying, setnowplaying] = useState(null);
+
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYjcxYjhjYmQ0ZjNjMGZmNzQ0NzQxZDQxYjA5Yzk1NyIsInN1YiI6IjY0YTE1ZjZmOGMwYTQ4MDEwMTc3MTFjMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KZQ4Di2xJs-cQ4L9ygCJ7z7tiXougY-i3VKE3l6Lt3Q',
+      },
+    };
+
+    //now playing
+    fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+      .then((response) => response.json())
+      .then((readabledata) => {
+        setnowplaying(readabledata);
+        console.log(readabledata);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  const displayBannerImage = () => {
+    if (nowplaying && nowplaying.results.length > 0) {
+      const obj = nowplaying.results[0];
+      return (
+        <div className="poster" key={obj.title}>
+          <img src={`https://image.tmdb.org/t/p/w500${obj.backdrop_path}`} alt="poster" />
+          <div className="content">
+            <h1 className="title">{obj.original_title}</h1>
+            <div  className="banner_button">
+              <button className="button">play</button>
+              <button className="button">Mylist</button>
             </div>
-            <p className='discription'>MONEY HEIST (known in Spanish as La casa de papel) Eight thieves take hostages and lock themselves in the Royal Mint of Spain as a criminal mastermind manipulates the police to carry out his plan</p>
+            <p className="description">
+              {obj.overview}
+            </p>
+          </div>
         </div>
+      );
+    } else {
+      return null;
+    }
+  };
+
+  return (
+    <div className="banner">
+      {displayBannerImage()}
       <div className="fade_bottom"></div>
     </div>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
